83c08e7fda271887b0d215e179e4cb49c968d8b5
Pooja Verma <pooja32verma@gmail.com>
2024-01-01 23:31:07 -0800

    learning typecasting by making an area calculator

---
+++ Area.py
@@ -0,0 +1,4 @@
+length = float(input("Enter the length of the rectangle: "))
+breadth = float(input("Enter the breadth of the rectangle: "))
+area = float(length * breadth)
+print(f"The area of rectangle with length {length}cm and breadth {breadth}cm is: {area:.2f}cm^2")
