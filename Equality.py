3d3ea06f47f6ce97f558cfb4a2e079f9158d9772
Pooja Verma <pooja32verma@gmail.com>
2024-01-01 23:35:26 -0800

    Check equality among two numbers

---
+++ Equality.py
@@ -0,0 +1,7 @@
+num1=int(input("Enter number 1: "))
+num2=int(input("Enter number 2: "))
+if num1 == num2:
+    print("Both the numbers are equal.")
+else:
+    print("Both the numbers are not equal.") 
+       
