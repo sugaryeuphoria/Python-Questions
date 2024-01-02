95e0f7750d9ae134455d4f3e3f59004783f76209
Pooja Verma <pooja32verma@gmail.com>
2024-01-01 23:39:53 -0800

    Largest number checker using functions

---
+++ Largest.py
@@ -0,0 +1,20 @@
+# a = int(input("Enter number 1 : "))
+# b = int(input("Enter number 2 : "))
+# c = int(input("Enter number 3 : "))
+
+# if a > b and a > c:
+#     print("a is the largest")3
+
+# elif b > a and b > c:
+#     print("b is the largest")
+# else:
+#     print("c is the largest")        
+
+# or
+a = int(input("Enter number 1 : "))
+b = int(input("Enter number 2 : "))
+c = int(input("Enter number 3 : "))
+
+largest=max(a,b,c)
+
+print("The largest number is : ",largest)
